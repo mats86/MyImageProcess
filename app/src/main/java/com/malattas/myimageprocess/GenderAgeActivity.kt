9a9675fe7f8b1276a0d859ab70af671703d91cba
mats86 <maa86.ats@gmail.com>
2019-03-25 08:33:05 +0100

    Init

---
+++ app/src/main/java/com/malattas/myimageprocess/GenderAgeActivity.kt
@@ -0,0 +1,12 @@
+package com.malattas.myimageprocess
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GenderAgeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gender_age)
+    }
+}
